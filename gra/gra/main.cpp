8cbd9aa482320be5a2dc6374d0e599af113fbef3
Kosstuch <piotrek.kostuszek@wp.pl>
2022-12-29 20:00:07 +0100

    3

--- gra/gra/main.cpp
+++ gra/gra/main.cpp
@@ -1,20 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-	return 0;
-}
+	srand(time(0));
+	RenderWindow app(VideoMode(520, 450), "Arkanoid!");
+	app.setFramerateLimit(60);
